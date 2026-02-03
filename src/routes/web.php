c78385d475da5f5c34915f4ec4dc189931b62be4
Mateus Silva <mateussilva@ntopus.com.br>
2026-02-03 14:33:15 -0300

    add prometheus and dashboard grafana to infra

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Prometheus\CollectorRegistry;
+use Prometheus\RenderTextFormat;
+use Prometheus\Storage\InMemory;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/metrics', function () {
+   $registry = new CollectorRegistry(new InMemory());
+   $renderer = new RenderTextFormat();
+
+   return response(
+       $renderer->render($registry->getMetricFamilySamples()),
+       200,
+       ['Content-Type' => RenderTextFormat::MIME_TYPE]
+   );
 });
