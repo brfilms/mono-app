775c512e8bbe449dbaf310166532ee38d313a7bb
Leticia Melo <leticiamelodia@gmail.com>
2026-03-08 20:15:10 -0300

    Create operator

---
+++ src/resources/views/operators/create_operator.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="{{route('operators')}}" method="post">
+        @csrf
+        <label for="name">Nome: </label>
+        <input type="text" name="name" id="name">
+        <br>
+        <label for="document">CPF: </label>
+        <input type="number" name="document" id="document">
+        <br>
+        <label for="birthdate">Data de Nascimento: </label>
+        <input type="date" name="birthdate" id="birthdate">
+        <br>
+        <label for="registration">Nº Matrícula: </label>
+        <input type="number" name="registration" id="registration">
+        <br>
+        <label for="phone">Telefone: </label>
+        <input type="number" name="phone" id="phone">
+        <br>
+        <label for="address">Endereço: </label>
+        <input type="text" name="address" id="address">
+        <br>
+        <label for="sector">Setor: </label>
+        <input type="text" name="sector" id="sector">
+        <br>
+        <label for="email">Email: </label>
+        <input type="text" name="email" id="email">
+        <br>
+        <label for="email_confirm">Confirme seu Email: </label>
+        <input type="text" name="email_confirm" id="email_confirm">
+        <br>
+        <label for="password">Senha: </label>
+        <input type="password" name="password" id="password">
+        <br>
+        <label for="password_confirm">Confirme sua senha: </label>
+        <input type="password" name="password_confirm" id="password_confirm">
+        <br>
+        <button type="submit">Cadastrar</button>
+    </form>
+</body>
+</html>
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\UserController;
+use App\Models\Operator;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Prometheus\CollectorRegistry;
 use Prometheus\RenderTextFormat;
@@ -9,3 +11,24 @@ use Prometheus\Storage\InMemory;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/operators/create', function () {
+    return view("operators.create_operator");
+})->name('operators.create');
+
+Route::post('/operators', function(Request $request) {
+    $body = $request->post();
+    $operator = new Operator();
+    $operator->name = $body['name'];
+    $operator->document = $body['document'];
+    $operator->birthdate = $body['birthdate'];
+    $operator->registration = $body['registration'];
+    $operator->phone = $body['phone'];
+    $operator->address = $body['address'];
+    $operator->sector = $body['sector'];
+    $operator->email = $body['email'];
+    $operator->password = $body['password'];
+    $operator->save();
+
+    return redirect('/operators/create');
+})->name('operators');
